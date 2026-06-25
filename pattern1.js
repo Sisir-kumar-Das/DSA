830929be7b0f8f29082803e711b20025b967645c
Sisir <das1234sisir@gmail.com>
2026-06-25 11:42:04 +0530

    dynamic pattern1

--- pattern1.js
+++ pattern1.js
@@ -1,6 +1,17 @@
-for (let i = 0; i < 4; i++) {
-  for (let j = 0; j < 4; j++) {
-    process.stdout.write("*");
+const pattern1 = (n) => {
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      process.stdout.write("* ");
+    }
+    console.log("");
   }
-  console.log("");
-}
+};
+
+const main = () => {
+  const fs = require("fs");
+  const input = fs.readFileSync(0, "utf8").trim().split("\n");
+
+  const n = parseInt(input[0]);
+  pattern1(n);
+};
+main();
