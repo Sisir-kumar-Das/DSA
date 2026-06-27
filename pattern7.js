74e252187a845f677e7f9ec1b44febd10c9bcd3b
Sisir <das1234sisir@gmail.com>
2026-06-27 11:00:35 +0530

    pattern 7

---
+++ pattern7.js
@@ -0,0 +1,17 @@
+const pattern7 = (n) => {
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= n - i + 1; j++) {
+      process.stdout.write(String(j) + " ");
+    }
+    console.log("");
+  }
+};
+
+const main = () => {
+  const fs = require("fs");
+  const input = fs.readFileSync(0, "utf8").trim().split("\n");
+
+  const n = parseInt(input[0]);
+  pattern7(n);
+};
+main();
