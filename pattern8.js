fcd48053df92c1824156990f211f2d4a7c025c09
Sisir <das1234sisir@gmail.com>
2026-06-29 10:51:51 +0530

    pattern 8 (bit diff)

---
+++ pattern8.js
@@ -0,0 +1,27 @@
+const pattern8 = (n) => {
+  for (let i = 0; i < n; i++) {
+    //space
+    for (let j = 0; j < n - i - 1; j++) {
+      process.stdout.write(" ");
+    }
+    //star
+    for (let j = 0; j < 2 * i + 1; j++) {
+      process.stdout.write("*");
+    }
+    //space
+    for (let j = 0; j < n - i - 1; j++) {
+      process.stdout.write(" ");
+    }
+    console.log("");
+  }
+};
+//
+
+const main = () => {
+  const fs = require("fs");
+  const input = fs.readFileSync(0, "utf8").trim().split("\n");
+
+  const n = parseInt(input[0]);
+  pattern8(n);
+};
+main();
