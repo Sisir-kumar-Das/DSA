ab4297a684a833247b49d0c638a0968e4d43cab1
Sisir <das1234sisir@gmail.com>
2026-07-03 11:28:05 +0530

    pattern 20 square

---
+++ pattern20.js
@@ -0,0 +1,23 @@
+const pattern20 = (n) => {
+  for (let i = 0; i < n; i++) {
+    //stars
+    for (let j = 0; j < n; j++) {
+      if (i === 0 || i === n - 1 || j === 0 || j === n - 1) {
+        process.stdout.write("*");
+      } else {
+        process.stdout.write(" ");
+      }
+    }
+    console.log("");
+  }
+};
+//
+
+const main = () => {
+  const fs = require("fs");
+  const input = fs.readFileSync(0, "utf8").trim().split("\n");
+
+  const n = parseInt(input[0]);
+  pattern20(n);
+};
+main();
