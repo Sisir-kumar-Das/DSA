c0cbc0b189aa616aa35adeb7db9236d30a6c2cd5
Sisir <das1234sisir@gmail.com>
2026-07-22 10:57:29 +0530

    Bubble sort

---
+++ Sorting/bubbleSort.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // Write C++ code here
+    int n, mini = 0;
+    cout << "Enter the number of array elements you want: ";
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Enter the value at array index " << i + 1 << " : ";
+        cin >> arr[i];
+    }
+
+    // Bubble sort
+    for (int i = n - 1; i >= 1; i--)
+    {
+        for (int j = 0; j <= i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    cout << "Sorted array elements are: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
