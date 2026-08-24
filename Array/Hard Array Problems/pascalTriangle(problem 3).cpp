8a7cb3220213d76c27931d86b03509b335938e78
Sisir <das1234sisir@gmail.com>
2026-08-24 09:55:31 +0530

    pascal triangle changes

--- Array/Hard Array Problems/pascalTriangle(problem 3).cpp
+++ Array/Hard Array Problems/pascalTriangle(problem 3).cpp
@@ -3,8 +3,23 @@ using namespace std;
 
 vector<vector<int>> pascalTriangle(int n)
 {
-    long long res = 1;
     vector<vector<int>> ans;
+
+    for (int row = 0; row < n; row++)
+    {
+        vector<int> currentRow(row + 1);
+        long long res = 1;
+        currentRow[0] = 1;
+
+        for (int col = 1; col <= row; col++)
+        {
+            // nCr formula built incrementally: res = res * (row - col + 1) / col
+            res = res * (row - col + 1) / col;
+            currentRow[col] = (int)res;
+        }
+
+        ans.push_back(currentRow);
+    }
 
     return ans;
 }
