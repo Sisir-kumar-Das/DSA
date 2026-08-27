a60a461ec29e26a005f455ff7f3c5740f103c2f7
Sisir <das1234sisir@gmail.com>
2026-08-27 11:18:42 +0530

    4 sum problem optimal

--- Array/Hard Array Problems/3sum.cpp
+++ Array/Hard Array Problems/3sum.cpp
@@ -1,32 +1,63 @@
-class Solution {
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
+    vector<vector<int>> threeSum(vector<int> &nums)
+    {
         int n = nums.size();
-        vector <vector<int>> ans;
+        vector<vector<int>> ans;
         sort(nums.begin(), nums.end());
-        for(int i =0 ; i< n;i++){
-            if(i>0 && nums[i] == nums[i-1]) continue;
-            int j = i+1;
-            int k = n-1;
-            while(j<k){
+        for (int i = 0; i < n; i++)
+        {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            int j = i + 1;
+            int k = n - 1;
+            while (j < k)
+            {
                 int sum = nums[i] + nums[j] + nums[k];
-                if(sum < 0){
+                if (sum < 0)
+                {
                     j++;
                 }
-                else if(sum > 0){
+                else if (sum > 0)
+                {
                     k--;
                 }
-                else{
-                    vector <int> temp = {nums[i], nums[j], nums[k]};
+                else
+                {
+                    vector<int> temp = {nums[i], nums[j], nums[k]};
                     ans.push_back(temp);
                     j++;
                     k--;
-                    while(j<k && nums[j] == nums[j-1]) j++;
-                    while(j<k && nums[k] == nums[k+1]) k--;
+                    while (j < k && nums[j] == nums[j - 1])
+                        j++;
+                    while (j < k && nums[k] == nums[k + 1])
+                        k--;
                 }
             }
-
         }
-        return ans;        
+        return ans;
     }
 };
+
+// Optional: driver code to test locally
+int main()
+{
+    Solution sol;
+    vector<int> nums = {-1, 0, 1, 2, -1, -4};
+
+    vector<vector<int>> result = sol.threeSum(nums);
+
+    for (auto &triplet : result)
+    {
+        cout << "[ ";
+        for (int val : triplet)
+            cout << val << " ";
+        cout << "]" << endl;
+    }
+
+    return 0;
+}
